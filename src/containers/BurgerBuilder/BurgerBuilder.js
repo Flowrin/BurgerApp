991abed6854e60720f9e7a403acf313eb4b02f68
Florin Lilea <florin.lilea94@gmail.com>
2020-11-12 14:58:12 +0100

    ingredients  code

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Aux from '../../hoc/Auxiliary';
+class BurgerBuilder extends Component {
+  render() {
+    return (
+      <Aux>
+        <div>Burger</div>
+        <div>Burer Controls</div>
+      </Aux>
+    );
+  }
+}
+export default BurgerBuilder;
